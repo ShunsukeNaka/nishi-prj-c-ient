7442b05b476911335e4b5f323a82cbf89ab44e20
ShunsukeNaka <s.nak81.0510@gmail.com>
2026-01-06 10:00:36 +0900

    add home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,38 @@
-import PageTitle from '.././components/ui/PageTitle'
+import PersonIcon from '@mui/icons-material/Person';
+import { Card, CardActionArea, Stack, Typography } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate();
 
   return (
     <>
-      <PageTitle text={"Home"}/>
+      <Stack direction="column">
+        <Card sx={{ width: "300px", height: "250px", margin: "20px" }}>
+          <CardActionArea
+            sx={{ textAlign: "center", height: "100%" }}
+            onClick={() => navigate("/mypage")}
+          >
+            <Typography sx={{ paddingTop: "20px", fontSize: "36px" }}>
+              まいぺーじ
+            </Typography>
+            <PersonIcon sx={{ fontSize: 150 }} />
+          </CardActionArea>
+        </Card>
+
+        <Card sx={{ width: "300px", height: "300px", margin: "20px" }}>
+          <CardActionArea
+            sx={{ textAlign: "center", height: "100%" }}
+            onClick={() => navigate("/diagnosis_menu")}
+          >
+            <Typography sx={{ fontSize: "36px" }}>
+              診断
+            </Typography>
+          </CardActionArea>
+        </Card>
+      </Stack>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
