d60a0b0bc8bb79afb954e819512092e8eda8dbde
ShunsukeNaka <s.nak81.0510@gmail.com>
2026-01-06 14:34:25 +0900

    pudding PageTitle

--- src/components/ui/PageTitle.tsx
+++ src/components/ui/PageTitle.tsx
@@ -4,7 +4,7 @@ export default function PageTitle(props: {text: string}) {
     
     return (
         <>
-            <Typography sx={{color: "white", fontSize: 35, textAlign: "center"}}>
+            <Typography sx={{color: "white", fontSize: 35, textAlign: "center", padding: 2, paddingBottom: "40px"}}>
                 {props.text}
             </Typography>
         </>
